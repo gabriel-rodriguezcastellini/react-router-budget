2e343857385ff117a4ec4d57cfa01c67f4aa9c2c
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-01-12 17:40:54 -0300

    Add routing and dashboard components with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Dashboard, { dashboardLoader } from "./Pages/Dashboard"
+import Error from "./Pages/Error"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />
+  }
+])
+
 function App() {  
-  return <div className="App"></div>
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>
 }
 
 export default App
