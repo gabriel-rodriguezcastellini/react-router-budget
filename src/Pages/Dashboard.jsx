83f52027775f946c9353115485d069289b71b75e
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-01-15 17:40:04 -0300

    Add dashboard action and intro component for user account creation

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,19 +1,28 @@
 import { useLoaderData } from "react-router-dom";
-import { fetchData } from "../helpers"
+import { fetchData } from "../helpers";
+import Intro from "../components/Intro";
+import { toast } from "react-toastify";
 
-export function dashboardLoader(){
-    const userName = fetchData("userName");
-    return { userName };
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    // localStorage.setItem("userName", JSON.stringify(formData.userName));
+    // return toast.success(`Welcome, ${formData.userName}`);
+    throw new Error("Ya done");
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
 }
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  )
-}
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+};
 
-export default Dashboard
+export default Dashboard;
