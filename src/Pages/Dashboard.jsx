33637278d6e7d51c6556d51f25695e177e09b870
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-01-17 15:50:01 -0300

    Add budget creation form to dashboard and update data loading

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,10 +2,12 @@ import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helpers";
 import Intro from "../components/Intro";
 import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+  return { userName, budgets };
 }
 
 export async function dashboardAction({ request }) {
@@ -20,8 +22,28 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  const { userName, budgets } = useLoaderData();
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
