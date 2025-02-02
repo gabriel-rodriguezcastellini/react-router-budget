0d109f4410cb6411f5bb0b39477c9ae2add36c25
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-02-02 16:44:10 -0300

    Add BudgetItem component and display existing budgets in the dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import Intro from "../components/Intro";
 import { toast } from "react-toastify";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
@@ -60,22 +61,28 @@ const Dashboard = () => {
           <h1>
             Welcome back, <span className="accent">{userName}</span>
           </h1>
-          {budgets && budgets.length > 0 ? (
-            <div className="grid-sm">
+          <div className="grid-sm">
+            {budgets && budgets.length > 0 ? (
               <div className="grid-lg">
                 <div className="flex-lg">
                   <AddBudgetForm />
                   <AddExpenseForm budgets={budgets} />
                 </div>
+                <h2>Existing Budgets</h2>
+                <div className="budgets">
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="grid-sm">
-              <p>Personal budgeting is the secret to financial freedom.</p>
-              <p>Create a budget to get started!</p>
-              <AddBudgetForm />
-            </div>
-          )}
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secret to financial freedom.</p>
+                <p>Create a budget to get started!</p>
+                <AddBudgetForm />
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <Intro />
