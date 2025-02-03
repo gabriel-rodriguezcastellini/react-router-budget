aaef462c047d2e678a29874383cddb9208150ca9
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-02-03 16:50:07 -0300

    Enhance BudgetItem component to display budget details and progress

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const BudgetItem = ({ budget }) => {
-  return <div>{budget.name}</div>;
+  const { id, name, amount, color } = budget;
+  return (
+    <div className="budget">
+      <div className="progress-text">
+        <h3>{name}</h3>
+        <p>{amount} Budgeted</p>
+      </div>
+      <progress max={amount} value="100"></progress>
+      <div className="progress-text">
+        <small>... spent</small>
+        <small>... remaining</small>
+      </div>
+    </div>
+  );
 };
 
 export default BudgetItem;
