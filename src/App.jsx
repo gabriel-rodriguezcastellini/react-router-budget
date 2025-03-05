831e596bed44bca9a49a67f1c7a19579a6463625
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-05 16:55:03 -0300

    Add budget loader to BudgetPage and display budget name

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ExpensesPage, {
   expensesAction,
   expensesLoader,
 } from "./pages/ExpensesPage";
-import BudgetPage from "./pages/BudgetPage";
+import BudgetPage, { budgetLoader } from "./pages/BudgetPage";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
       {
         path: "budget/:id",
         element: <BudgetPage />,
+        loader: budgetLoader,
       },
       {
         path: "expenses",
--- src/Pages/BudgetPage.jsx
+++ src/Pages/BudgetPage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { getAllMatchingItems } from "../helpers";
+import { useLoaderData } from "react-router-dom";
 
 export async function budgetLoader({ params }) {
-  const budget = await getAllMatchingItems(
-    {
-      category: "budgets",
-      key: "id",
-      value: params.id,
-    },
-    [0]
-  );
+  const budget = await getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: params.id,
+  })[0];
+  return { budget };
 }
 
 const BudgetPage = () => {
-  return <div>BudgetPage</div>;
+  const { budget } = useLoaderData();
+  return <div>{budget.name}</div>;
 };
 
 export default BudgetPage;
