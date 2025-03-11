418e88fcbf8cf8dc0f75ec2f362138cf51b89b3c
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-10 21:59:06 -0300

    Add AddExpenseForm to BudgetPage and load associated expenses

--- src/Pages/BudgetPage.jsx
+++ src/Pages/BudgetPage.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { getAllMatchingItems } from "../helpers";
 import { useLoaderData } from "react-router-dom";
 import BudgetItem from "../components/BudgetItem";
+import AddExpenseForm from "../components/AddExpenseForm";
 
 export async function budgetLoader({ params }) {
   const budget = await getAllMatchingItems({
     category: "budgets",
     key: "id",
+    value: params.id,
+  })[0];
+  const expenses = await getAllMatchingItems({
+    category: "expenses",
+    key: "budgetId",
     value: params.id,
   })[0];
   if (!budget) {
@@ -24,6 +30,7 @@ const BudgetPage = () => {
       </h1>
       <div className="flex-lg">
         <BudgetItem budget={budget} />
+        <AddExpenseForm budgets={[budget]} />
       </div>
     </div>
   );
