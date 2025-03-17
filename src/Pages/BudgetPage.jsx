e1ac0657f367bd675181f048360442ca44a9070d
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-17 20:53:23 -0300

    Add createExpense action to BudgetPage for adding new expenses

--- src/Pages/BudgetPage.jsx
+++ src/Pages/BudgetPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { deleteItem, getAllMatchingItems } from "../helpers";
+import { createExpense, deleteItem, getAllMatchingItems } from "../helpers";
 import { useLoaderData } from "react-router-dom";
 import BudgetItem from "../components/BudgetItem";
 import AddExpenseForm from "../components/AddExpenseForm";
@@ -26,6 +26,19 @@ export async function budgetLoader({ params }) {
 export async function budgetAction({ request }) {
   const data = await request.formData();
   const { _action, ...values } = Object.fromEntries(data);
+
+  if (_action === "createExpense") {
+    try {
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget,
+      });
+      return toast.success(`Expense ${values.newExpense} created!`);
+    } catch (e) {
+      throw new Error("There was a problem creating your expense.");
+    }
+  }
 
   if (_action === "deleteExpense") {
     try {
