6609a77a22fcb425b8d12d64e44b99d6f9a2f3a3
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-18 20:56:55 -0300

    Add showDelete prop to BudgetItem component for conditional delete display

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -5,7 +5,7 @@ import {
   formatPercentage,
 } from "../helpers";
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
   return (
