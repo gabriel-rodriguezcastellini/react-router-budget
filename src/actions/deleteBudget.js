b9474fd07c1efbeef5e3886e942727ff9649e630
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-26 20:04:24 -0300

    Refactor deleteBudget action to utilize deleteItem helper for improved deletion logic

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -1,3 +1,8 @@
-export function deleteBudget() {
-  console.log("deleting");
+import { deleteItem } from "../helpers";
+
+export function deleteBudget({ params }) {
+  deleteItem({
+    key: "budgets",
+    id: params.id,
+  });
 }
