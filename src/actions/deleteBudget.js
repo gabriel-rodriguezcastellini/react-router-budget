1e66a3bebc2f3fa88123406dd9ef8c29fbf34c5f
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-03-28 14:10:17 -0300

    Add functionality to retrieve associated expenses when deleting a budget

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -1,11 +1,16 @@
 import { toast } from "react-toastify";
-import { deleteItem } from "../helpers";
+import { deleteItem, getAllMatchingItems } from "../helpers";
 
 export function deleteBudget({ params }) {
   try {
     deleteItem({
       key: "budgets",
       id: params.id,
+    });
+    const associatedExpenses = getAllMatchingItems({
+      category: "expenses",
+      key: "budgetId",
+      value: params.id,
     });
     toast.success("Budget deleted successfully!");
   } catch (error) {
